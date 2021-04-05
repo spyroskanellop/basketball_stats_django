da01bfb7c6cfeb16b781410a969683a00d4820f2
spyros.kanellop <spyroskanellop@gmail.com>
2021-04-05 13:32:03 +0300

    Added full functionality to team class in crud. #17 Fixed redirect issue

--- modelview/urls.py
+++ modelview/urls.py
@@ -5,9 +5,12 @@ from run import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('team/addTeam', views.team_form),
-    path('conf', views.admin),
-    path('home', views.home),
-    path('team/showTeams', views.team_list),
+    path('team/addTeam', views.createTeam, name='team_insert'),
+    path('team/showTeams', views.team_list, name='team_list'),
+    # path('team/updateTeam/'),
+    path('update_team/<int:id>/', views.updateTeam, name='team_update'),
+    path('delete_team/<int:id>/', views.deleteTeam, name='team_delete'),
+
+    path('', views.home, name='home'),
     # path('player/addPlayer', views.player_form)
 ]
--- run/views.py
+++ run/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import path
 from django.shortcuts import render, redirect
 from run.models import Teams
@@ -7,42 +7,65 @@ from .forms import TeamsForm, PlayersForm, ScoresForm
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello from home')
+    return render(request, 'admin.html')
 
-def team(request):
-    if request.method == 'POST':
-        form = TeamsForm(request.POST)
-        if form.is_valid():
-            try:
-                form.save()
-                return redirect('/view')
-            except:
-                pass
-    else:
-        form = TeamsForm()
-    return render(request, 'teams_form.html', {'form':form})
+# def team(request):
+#     if request.method == 'POST':
+#         form = TeamsForm(request.POST)
+#         if form.is_valid():
+#             try:
+#                 form.save()
+#                 return redirect('/view')
+#             except:
+#                 pass
+#     else:
+#         form = TeamsForm()
+#     return render(request, 'teams_form.html', {'form':form})
 
-def team_form(request):
+# TODO needs further fixing
+def createTeam(request):
     form = TeamsForm()
     if request.method == 'POST':
         form = TeamsForm(request.POST)
         if form.is_valid():
             print('saved')
             form.save()
+            return HttpResponseRedirect('showTeams')
         else:
             print('not saved')
-    else:
-        print('not inside post')
     context = {'form': form}
-
     return render(request, "teams_form.html", context)
+
+def updateTeam(request, id):
+    team = Teams.objects.get(pk=id)
+    form = TeamsForm(instance=team)
+    if request.method == 'POST':
+        form = TeamsForm(request.POST, instance=team)
+        if form.is_valid():
+            print('saved')
+            form.save()
+            return HttpResponseRedirect('/../../team/showTeams')
+        else:
+            print('not updated')
+
+    context = {'form': form}
+    return render(request, "teams_form.html", context)
+
 
 def team_list(request):
     context = {'team_list':Teams.objects.all()}
     return render(request, "teams_list.html", context)
 
-def team_del(request):
-    return render(request, "teams_list.html")
+def deleteTeam(request, id):
+    team = Teams.objects.get(pk=id)
+    print(team)
+    context = {'team':team}
+
+    if request.method == "POST":
+        team.delete()
+        return HttpResponseRedirect('/../../team/showTeams')
+
+    return render(request, "delete_team.html", context)
 
 # def player_form(request):
 #     form = PlayersForm()
